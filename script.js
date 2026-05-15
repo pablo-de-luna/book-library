a1988f2dcd2d98d72306607405b345277e315499
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-05-15 17:01:26 -0600

    Add code for book elements and set attributes

--- script.js
+++ script.js
@@ -2,24 +2,53 @@
 
 "use strict"
 
+const libraryContainer = document.querySelector("#library-container");
+
+//Code to create book elements and attributes
+    const bookContainer = document.createElement("div");
+    bookContainer.setAttribute("class", "book-container")
+
+    const bookCover = document.createElement("div");
+    bookCover.setAttribute("class", "book-cover")
+
+    const coverImage = document.createElement("img");
+    // If there is no image, display solid color cover with title name text
+    coverImage.setAttribute("src", "");
+    coverImage.setAttribute("alt", "book cover image");
+    coverImage.setAttribute("height", "90");
+    coverImage.setAttribute("width", "60");
+
+    const bookTitle = document.createElement("div");
+    bookTitle.setAttribute("class", "book-title")
+
+    bookCover.appendChild(coverImage)
+    bookContainer.appendChild(bookCover)
+    bookContainer.appendChild(bookTitle)
+    libraryContainer.appendChild(bookContainer);
+
 const library = [];
 
-function Book(id, name, autor, pages) {
+function Book(name, autor, pages) {
   if (!new.target) throw Error(`Must use "new" operator`)
 
-  this.id = id;
+  this.id = crypto.randomUUID();
   this.name = name;
   this.autor = autor;
   this.pages = pages;
 }
 
 const addBookToLibrary = (name, autor, pages) => {
-  const bookID = crypto.randomUUID();
-  const theBook = new Book(bookID, name, autor, pages);
+  const theBook = new Book(name, autor, pages);
 
   library.push(theBook);
 }
-// addBookToLibrary("The Book", "Bookert Bookington", 324);
-// addBookToLibrary("King Book", "Robook Hamilbook", 215);
 
-// console.table(library)
+
+// BOOKS TO ADD
+addBookToLibrary("Of Mice and Men", "John Steinback", 107);
+addBookToLibrary("The Call of the Wild", "Jack London", 172);
+addBookToLibrary("1984", "George Orwell", 298);
+addBookToLibrary("White Fang", "Jack London", 252);
+addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 409);
+
+console.table(library)
