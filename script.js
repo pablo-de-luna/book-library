0b5463202433c93d2476937125e315fa61244f1e
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-05-15 12:32:02 -0600

    Add library array to store book objects
    created a constructor function to create book instances
    Made a function that store new book instances in the library array

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // THE LORD IS MY SHEPHERD, I LACK NOTHING
 
 "use strict"
+
+const library = [];
+
+function Book(name, autor, pages) {
+  this.name = name;
+  this.autor = autor;
+  this.pages = pages;
+}
+
+const addBookToLibrary = (name, autor, pages) => {
+  const book = new Book(name, autor, pages);
+
+  library.push(book);
+}
+
+addBookToLibrary("book name", "autor name", 324);
+addBookToLibrary("book2 name", "autor2", 215);
+
+console.log(library)
