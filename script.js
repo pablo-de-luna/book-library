45624285e2eb4e1b357b4ed86701096ffbc13f1a
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-05-15 18:09:57 -0600

    Add example books in library
    Added some book objects examples, with cover and title displayed in
    the library container

--- script.js
+++ script.js
@@ -4,51 +4,59 @@
 
 const libraryContainer = document.querySelector("#library-container");
 
-//Code to create book elements and attributes
-    const bookContainer = document.createElement("div");
-    bookContainer.setAttribute("class", "book-container")
+const createBookCard = (title) => {
+  const bookContainer = document.createElement("div");
+  bookContainer.setAttribute("class", "book-container")
 
-    const bookCover = document.createElement("div");
-    bookCover.setAttribute("class", "book-cover")
+  const bookCover = document.createElement("div");
+  bookCover.setAttribute("class", "book-cover")
 
-    const coverImage = document.createElement("img");
-    // If there is no image, display solid color cover with title name text
-    coverImage.setAttribute("src", "");
-    coverImage.setAttribute("alt", "book cover image");
-    coverImage.setAttribute("height", "90");
-    coverImage.setAttribute("width", "60");
+  const coverImage = document.createElement("img");
+  // If there is no image, display solid color cover with title name text
+  coverImage.setAttribute("src", "./assets/images/of-mice-and-men-cover.jpg");
+  coverImage.setAttribute("alt", "book cover image");
+  coverImage.setAttribute("height", "180");
+  coverImage.setAttribute("width", "120");
 
-    const bookTitle = document.createElement("div");
-    bookTitle.setAttribute("class", "book-title")
+  const bookTitle = document.createElement("div");
+  bookTitle.setAttribute("class", "book-title")
+  bookTitle.textContent = title;
 
-    bookCover.appendChild(coverImage)
-    bookContainer.appendChild(bookCover)
-    bookContainer.appendChild(bookTitle)
-    libraryContainer.appendChild(bookContainer);
+  bookCover.appendChild(coverImage)
+  bookContainer.appendChild(bookCover)
+  bookContainer.appendChild(bookTitle)
+  libraryContainer.appendChild(bookContainer);
+};
 
 const library = [];
 
-function Book(name, autor, pages) {
+function Book(title, autor, pages) {
   if (!new.target) throw Error(`Must use "new" operator`)
 
   this.id = crypto.randomUUID();
-  this.name = name;
+  this.title = title;
   this.autor = autor;
   this.pages = pages;
 }
 
-const addBookToLibrary = (name, autor, pages) => {
-  const theBook = new Book(name, autor, pages);
+const addBookToLibrary = (title, autor, pages) => {
+  const book = new Book(title, autor, pages);
 
-  library.push(theBook);
-}
+  library.push(book);
+};
 
+const addExampleBooks = () => {
+  addBookToLibrary("Of Mice and Men", "John Steinback", 107);
+  addBookToLibrary("The Call of the Wild", "Jack London", 172);
+  addBookToLibrary("1984", "George Orwell", 298);
+  addBookToLibrary("White Fang", "Jack London", 252);
+  addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 409);
+};
 
-// BOOKS TO ADD
-addBookToLibrary("Of Mice and Men", "John Steinback", 107);
-addBookToLibrary("The Call of the Wild", "Jack London", 172);
-addBookToLibrary("1984", "George Orwell", 298);
-addBookToLibrary("White Fang", "Jack London", 252);
-addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 409);
+addExampleBooks();
 
-console.table(library)
+for(let book of library) {
+  createBookCard(book.title)
+};
+
+console.log(library);
