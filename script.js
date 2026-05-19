c4c394c696daf38fa11d4adca20dfe686640d1f4
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-05-19 14:00:03 -0600

    Add function to assign cover images
    If there is no cover in the book object, a default book cover is shown

--- script.js
+++ script.js
@@ -2,9 +2,35 @@
 
 "use strict"
 
-const libraryContainer = document.querySelector("#library-container");
+const bookCardContainer = document.querySelector("#book-cards-container");
 
-const createBookCard = (title) => {
+const library = [];
+
+function Book(title, autor, pages, cover) {
+  if (!new.target) throw Error(`Must use "new" operator`)
+
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.autor = autor;
+  this.pages = pages;
+  this.cover = cover;
+}
+
+const addBookToLibrary = (title, autor, pages, cover) => {
+  const book = new Book(title, autor, pages, cover);
+
+  library.push(book);
+};
+
+const addExampleBooks = () => {
+  addBookToLibrary("Of Mice and Men", "John Steinback", 107, "of-mice-and-men-cover.jpg");
+  addBookToLibrary("The Call of the Wild", "Jack London", 172);
+  addBookToLibrary("1984", "George Orwell", 298);
+  addBookToLibrary("White Fang", "Jack London", 252);
+  addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 409);
+};
+
+const createBookCard = (title, cover) => {
   const bookContainer = document.createElement("div");
   bookContainer.setAttribute("class", "book-container")
 
@@ -12,8 +38,7 @@ const createBookCard = (title) => {
   bookCover.setAttribute("class", "book-cover")
 
   const coverImage = document.createElement("img");
-  // If there is no image, display solid color cover with title name text
-  coverImage.setAttribute("src", "./assets/images/of-mice-and-men-cover.jpg");
+  coverImage.setAttribute("src", assignCover(cover));
   coverImage.setAttribute("alt", "book cover image");
   coverImage.setAttribute("height", "180");
   coverImage.setAttribute("width", "120");
@@ -25,38 +50,22 @@ const createBookCard = (title) => {
   bookCover.appendChild(coverImage)
   bookContainer.appendChild(bookCover)
   bookContainer.appendChild(bookTitle)
-  libraryContainer.appendChild(bookContainer);
+  bookCardContainer.appendChild(bookContainer);
 };
 
-const library = [];
-
-function Book(title, autor, pages) {
-  if (!new.target) throw Error(`Must use "new" operator`)
-
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.autor = autor;
-  this.pages = pages;
+const assignCover = (cover) => {
+  if (!cover) {
+    return "./assets/images/default-cover.jpg";
+  } else {
+    return `./assets/images/${cover}`;
+  }
 }
 
-const addBookToLibrary = (title, autor, pages) => {
-  const book = new Book(title, autor, pages);
-
-  library.push(book);
-};
-
-const addExampleBooks = () => {
-  addBookToLibrary("Of Mice and Men", "John Steinback", 107);
-  addBookToLibrary("The Call of the Wild", "Jack London", 172);
-  addBookToLibrary("1984", "George Orwell", 298);
-  addBookToLibrary("White Fang", "Jack London", 252);
-  addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 409);
+const showBookCards = () => {
+  for(let book of library) {
+    createBookCard(book.title, book.cover)
+  }
 };
 
 addExampleBooks();
-
-for(let book of library) {
-  createBookCard(book.title)
-};
-
-console.log(library);
+showBookCards();
